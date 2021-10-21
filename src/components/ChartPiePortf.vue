f1833c85c99d77e9641c2f438d4b8359b7c8b0ea
EgorTrunov <88332602+EgorTrunov@users.noreply.github.com>
2021-10-21 19:11:38 +0300

    add chartPie

---
+++ src/components/ChartPiePortf.vue
@@ -0,0 +1,26 @@
+<template>
+  <DoughnutChart :chartData="testData" />
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+import { DoughnutChart } from 'vue-chart-3';
+
+export default defineComponent({
+  name: 'ChartPiePortf',
+  components: { DoughnutChart },
+  props: ['currenciesList'],
+  setup(props) {
+    const testData = {
+      labels: [props.currenciesList[0].title, props.currenciesList[1].title, props.currenciesList[2].title],
+      datasets: [
+        {
+          data: [props.currenciesList[0].value, props.currenciesList[1].value, props.currenciesList[2].value],
+          backgroundColor: ['#77CEFF', '#0079AF', '#123E6B'],
+        },
+      ],
+    };
+    return { testData };
+  },
+});
+</script>
